7b0022eab33553cbb5e147d63b1c22676d2220a1
Fandorin <SherJlok15@gmail.com>
2020-03-16 16:14:37 +0200

    Updates

---
+++ precache-manifest.3543c85d3479384efae54f43ff60486c.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dafd22ab357eb3ecfd9c55e970c98e2c",
+    "url": "/abz-agency/index.html"
+  },
+  {
+    "revision": "ba66cdf90b32a3e80fee",
+    "url": "/abz-agency/static/css/main.0b082a55.chunk.css"
+  },
+  {
+    "revision": "6ee3a76197fbee926f5e",
+    "url": "/abz-agency/static/js/2.95f610c9.chunk.js"
+  },
+  {
+    "revision": "425f71bb909b45cb21829b79476852ea",
+    "url": "/abz-agency/static/js/2.95f610c9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ba66cdf90b32a3e80fee",
+    "url": "/abz-agency/static/js/main.0c95f8b8.chunk.js"
+  },
+  {
+    "revision": "905ba33679f66314eb3f",
+    "url": "/abz-agency/static/js/runtime-main.daaa0484.js"
+  },
+  {
+    "revision": "3ed9575dcc488c3e3a5bd66620bdf5a4",
+    "url": "/abz-agency/static/media/OpenSans-Regular.3ed9575d.ttf"
+  },
+  {
+    "revision": "ba5cde21eeea0d57ab7efefc99596cce",
+    "url": "/abz-agency/static/media/OpenSans-SemiBold.ba5cde21.ttf"
+  },
+  {
+    "revision": "561fe7db4227410c8c16f27798d14b44",
+    "url": "/abz-agency/static/media/about-me_pikchure.561fe7db.svg"
+  },
+  {
+    "revision": "5f9b607f704da416d6e9690c71c804dd",
+    "url": "/abz-agency/static/media/banner_background.5f9b607f.png"
+  },
+  {
+    "revision": "ee15e9f17af98c2e948615422f9dbb15",
+    "url": "/abz-agency/static/media/logo.ee15e9f1.svg"
+  },
+  {
+    "revision": "ab4b97509130785003e35facbcaf55b3",
+    "url": "/abz-agency/static/media/open-menu.ab4b9750.svg"
+  }
+]);
