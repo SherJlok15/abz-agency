f3b27c098ba4452b41f999b6a1d18793c14db148
Fandorin <SherJlok15@gmail.com>
2020-03-16 16:29:58 +0200

    commit v.1

--- src/modules/Users/actions.js
+++ src/modules/Users/actions.js
@@ -168,6 +168,7 @@ const UsersActions = {
   },
 
   emailValidatorOnBlur: (value) => dispatch => {
+    // eslint-disable-next-line
     const re = /^(?:[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*|"(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21\x23-\x5b\x5d-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])*")@(?:(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?|\[(?:(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)\.){3}(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?|[a-z0-9-]*[a-z0-9]:(?:[\x01-\x08\x0b\x0c\x0e-\x1f\x21-\x5a\x53-\x7f]|\\[\x01-\x09\x0b\x0c\x0e-\x7f])+)\])$/
     if (re.test(value)) {
       dispatch(UsersActions.emailValid())
@@ -199,6 +200,7 @@ const UsersActions = {
   //-->> phone number validation
 
   phoneValidatorOnChange: (value) => dispatch => {
+    // eslint-disable-next-line
     const re = /^[\+]{0,1}380([0-9]{9})$/;
     const re2 = /^\\d{9}$/;
     dispatch(UsersActions.getUserPhoneValue(value));
